3ba69b0304f6df9c1b587b134979d8337153ad79
Yukitooo <yukibrent9@gmail.com>
2026-01-25 01:06:32 +0800

    created strucutre

---
+++ app/atomic/BasicButton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BasicButton() {
+  return (
+    <div>BasicButton</div>
+  )
+}
+
+export default BasicButton
--- app/atomic/GenerateButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function GenerateButton() {
-  return (
-    <div>GenerateButton</div>
-  )
-}
-
-export default GenerateButton
