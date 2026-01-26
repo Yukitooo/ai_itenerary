b5542976b4f5301e3ce826c8dbddb2b25b3b9fb7
Yukitooo <yukibrent9@gmail.com>
2026-01-26 18:45:48 +0800

    edited table page

--- app/pages/generatePage/Table.tsx
+++ app/pages/generatePage/Table.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
+import { ItineraryRow, Itinerary, ItineraryDay} from '@/app/types/types';
 
-const data = [
-    { name: "Anna", age: 23, city: "Manila" },
-    { name: "Mark", age: 30, city: "Cebu" },
+const data: ItineraryRow[] = [
+    { time: "10:00 - 11:20", place: "Baguio Strawberry Farm", itineraryRowDescription: "Pick some strawberries" },
+    { time: "2:00 - 4:30", place: "Baguio Lunch", itineraryRowDescription: "Eat very delicious soup" },
   ];
 
 function Table() {
+  
    return (
       <div className="w-full p-4 box-border">
       <table className="w-full border-collapse">
         <thead>
           <tr className="bg-gray-600">
             <th className="border border-gray-300 px-4 py-3 text-left">
-              Name
+              Time
             </th>
             <th className="border border-gray-300 px-4 py-3 text-left">
-              Age
+              Place
             </th>
             <th className="border border-gray-300 px-4 py-3 text-left">
-              City
+              Description
             </th>
           </tr>
         </thead>
@@ -27,13 +29,13 @@ function Table() {
           {data.map((item, index) => (
             <tr key={index} className="hover:bg-gray-50">
               <td className="border border-gray-300 px-4 py-3">
-                {item.name}
+                {item.time}
               </td>
               <td className="border border-gray-300 px-4 py-3">
-                {item.age}
+                {item.place}
               </td>
               <td className="border border-gray-300 px-4 py-3">
-                {item.city}
+                {item.itineraryRowDescription}
               </td>
             </tr>
           ))}
--- app/types/types.ts
+++ app/types/types.ts
@@ -1,5 +1,22 @@
+export type Plan = {
+  country: string;
+  dateRange: number;
+  planDescription: string;
+}
+
 export type ItineraryRow = {
-    time: string;
-    place: string;
-    itineraryRowDescription: string; 
+  time: string; 
+  place: string;
+  itineraryRowDescription: string;
+}
+
+export type ItineraryDay = {
+  currentDay: string;
+  itineraryRows: ItineraryRow[]; 
+}
+
+export type Itinerary = {
+  itineraryName: string;
+  itineraryDescription: string;
+  itineraryList: ItineraryDay[];
 }
