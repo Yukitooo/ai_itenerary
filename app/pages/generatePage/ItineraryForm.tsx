f0e2a1d30f73b57cb10264c874feb98cf0fc3ac3
oohnyx <nicolefranchescausi@gmail.com>
2026-01-30 02:25:48 +0800

    itinerary form naiwan sa push

--- app/pages/generatePage/ItineraryForm.tsx
+++ app/pages/generatePage/ItineraryForm.tsx
@@ -2,25 +2,42 @@
 import BasicButton from "@/app/atomic/BasicButton";
 import Input from "@/app/atomic/Input";
 import TextArea from "@/app/atomic/TextArea";
-import { Plan } from "@/app/types/types";
 import Dropdown from "@/app/atomic/Dropdown";
+import { usePlannerState } from "@/app/states/usePlannerState";
 
 function ItineraryForm() {
+  const {
+    country,
+    dateRange,
+    planDescription,
+    setCountry,
+    setDateRange,
+    setPlanDescription,
+  } = usePlannerState();
   const noOfDays = [1, 2, 3, 4, 5, 6, 7];
 
   return (
     <div className="flex flex-col gap-4 max-w-md">
-      <Input label="Country" placeholder="e.g. Japan" />
+      <Input
+        label="Country"
+        placeholder="e.g. Japan"
+        value={country}
+        onChange={setCountry}
+      />
 
       <Dropdown
         label="No. of Days"
         options={noOfDays}
         placeholder="Select duration of trip"
+        value={dateRange}
+        onChange={setDateRange}
       />
 
       <TextArea
         label="Describe your ideal itinerary"
         placeholder="I want my itinerary to be..."
+        value={planDescription}
+        onChange={setPlanDescription}
       />
 
       <BasicButton text="Generate Itinerary" />
